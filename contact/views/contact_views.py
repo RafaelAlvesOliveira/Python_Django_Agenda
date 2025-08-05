e83dccbf244db21f5c6108d56fb342f3d31dbdd6
Rafael Alves <faelalvesoliveira06@gmail.com>
2025-08-05 09:03:52 -0300

    Criando arquivos parciais para usar com include nos templates. Filtrando valores com Q e OR para o campo pesquisa.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,5 @@
-from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, render, redirect
 
 from contact.models import Contact
 
@@ -9,11 +9,37 @@ def index(request):
         .filter(show=True)\
         .order_by('id')[:10]
 
-    # print(contacts.query)
-    # Mostra as consultas SQL realizadas na tabela.
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(
@@ -29,12 +55,11 @@ def contact(request, contact_id):
         Contact.objects.filter(pk=contact_id),
         show=True,
     )
-
-    # if single_contact is None:
-    #     raise Http404()
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(
