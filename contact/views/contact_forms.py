cc59f4d9ac887bfd235531c2cce4d0341a0539e9
Rafael Alves <faelalvesoliveira06@gmail.com>
2025-08-10 00:29:03 -0300

    Continuação da criação do formulário dinâmico com forms.ModelFrom do Django. Movendo o ContactForm para forms.py.

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,17 +1,7 @@
-from django import forms
-from django.core.paginator import Paginator
-from django.db.models import Q
-from django.shortcuts import get_object_or_404, redirect, render
+# type: ignore
+from django.shortcuts import render
 
-from contact.models import Contact
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone',
-        )
+from contact.forms import ContactForm
 
 
 def create(request):
